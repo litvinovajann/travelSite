ad417e038094f0449949ccdb2b385ed9ec13a944
Janna <litvinovajann@gmail.com>
2023-08-05 10:20:18 -0700

    added data in server.js

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,6 +3,16 @@ import './main.css'
 
 export default function Main() {
     return (
-        <div> main </div>
+        <section className="main container">
+            <div className="secTitle">
+                <h3 className="title">
+                    Nost visited destinations
+                </h3>
+            </div>
+
+            <div className="secContent grid">
+                
+            </div>
+        </section>
     )
 }
